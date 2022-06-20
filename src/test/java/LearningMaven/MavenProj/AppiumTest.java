f173effec1800d6623f8aff1532be23c1056ddf1
ravi <raviikumar.vemula@gmail.com>
2022-06-20 21:19:33 +0530

    First Commit

---
+++ src/test/java/LearningMaven/MavenProj/AppiumTest.java
@@ -0,0 +1,15 @@
+package LearningMaven.MavenProj;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void appsAndroid() {
+		System.out.println("appsAndroid");
+	}
+	
+	@Test
+	public void appsIOS() {
+		System.out.println("appsIOS");
+	}
+}
---
+++ src/test/java/LearningMaven/MavenProj/SeleniumTest.java
@@ -0,0 +1,15 @@
+package LearningMaven.MavenProj;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void browserTest() {
+		System.out.println("browserTest");
+	}
+	
+	@Test
+	public void elementsUI() {
+		System.out.println("elementsUI");
+	}
+}
